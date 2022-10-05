2af361657eb15eafc3f485c1e5e9650f35b5349d
zihulanshi <58834622+zihulanshi@users.noreply.github.com>
2022-10-05 12:56:30 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -24,7 +24,7 @@
 
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
 #openclash
 #git clone https://github.com/vernesong/OpenClash.git package/OpenClash
